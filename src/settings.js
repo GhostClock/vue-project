65d17206b8b9572824f06203abf3d3d1dfcf6adf
GhostClock <zhangyuanlaifen@gmail.com>
2023-05-09 23:38:47 +0800

    模板文件的认识

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,18 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '后台管理系统',
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
+   * 是否要固定头部
    */
   fixedHeader: false,
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
+   * 左侧导航栏logo
    */
   sidebarLogo: false
 }
